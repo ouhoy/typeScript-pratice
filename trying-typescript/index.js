c9832cccbfecc04ba68a281cbdfb6af262148e52
ouhoy <kimkinon@gmail.com>
2023-01-27 22:36:30 +0100

    Convert TS to JS

---
+++ trying-typescript/index.js
@@ -0,0 +1,9 @@
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+button.addEventListener("click", function () {
+    console.log(add(Number(input1.value), Number(input2.value)));
+});
---
+++ trying-typescript/index.ts
@@ -0,0 +1,11 @@
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+button.addEventListener("click", function() {
+  console.log(add(Number(input1.value), Number(input2.value)));
+});
