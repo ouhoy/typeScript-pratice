8fc8a85425090e6f4c4180cbb85a9ee6dbefc129
ouhoy <kimkinon@gmail.com>
2023-02-08 16:28:59 +0100

    More about generics

--- generics/app.ts
+++ generics/app.ts
@@ -16,6 +16,7 @@ function merge<T extends object, U extends object>(objA: object, objB: object) {
 }
 
 const mergedObj = merge({name: "james"}, {age: 23})
+console.log(mergedObj)
 
 function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
 
@@ -25,7 +26,7 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 
 extractAndConvert({name: "TypeScript"}, "name")
 
-class DataStorage<T> {
+class DataStorage<T extends number | string> {
     private data: T[] = [];
 
     addItem(item: T) {
@@ -55,3 +56,29 @@ numberStorage.addItem(1337)
 numberStorage.addItem(101)
 numberStorage.removeItem(101)
 console.log(numberStorage.getItems())
+
+interface CourseGoal {
+
+    title: string,
+    description: string,
+    completeUnit: Date
+
+}
+
+function courseGoal(title: string, description: string, completeUnit: Date): CourseGoal {
+
+    // Partial allows my interface's properties to be optional
+    let courseGoal: Partial<CourseGoal> = {}
+
+    courseGoal.title = title
+    courseGoal.description = description
+    courseGoal.completeUnit = completeUnit
+
+    return <CourseGoal>courseGoal
+}
+
+courseGoal("TypeScript crash course.", "Learn TypeScript with this 15hrs course.", new Date())
+
+const lang: Readonly<string[]> = ["JS", "TS", "CPP"]
+
+console.log(lang)
