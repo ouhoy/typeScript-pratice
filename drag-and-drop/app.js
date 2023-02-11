b075dc68a28d9db0872ab8bbd41481a6dc4ff7ef
ouhoy <kimkinon@gmail.com>
2023-02-11 17:09:40 +0100

    Get user inputs

--- drag-and-drop/app.js
+++ drag-and-drop/app.js
@@ -1,4 +1,20 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autoBind(target, methodName, descriptor) {
+    var originalMethod = descriptor.value;
+    var adjustedDescriptor = {
+        configurable: true,
+        get: function () {
+            return originalMethod.bind(this);
+        }
+    };
+    return adjustedDescriptor;
+}
 function $(selector, selectAll) {
     if (selectAll === void 0) { selectAll = false; }
     return selectAll ? document.querySelectorAll(selector) : document.querySelector(selector);
@@ -6,15 +22,42 @@ function $(selector, selectAll) {
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
         this.templateElement = $("#project-input");
-        this.hostElement = $("#id");
+        this.hostElement = $("#app");
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
-        console.log(this.element);
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
+        this.configure();
         this.attach();
     }
+    ProjectInput.prototype.gatherUserInput = function () {
+        var enteredTitle = this.titleInputElement.value;
+        var enteredDescription = this.descriptionInputElement.value;
+        var enteredPeople = this.peopleInputElement.value;
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || Number(enteredPeople) < 0) {
+            return alert("Please enter a valid input!");
+        }
+        return [enteredTitle, enteredDescription, Number(enteredPeople)];
+    };
+    ProjectInput.prototype.submitHandler = function (event) {
+        event.preventDefault();
+        var userInput = this.gatherUserInput();
+        if (userInput) {
+            var title = userInput[0], description = userInput[1], people = userInput[2];
+            console.log(title, description, people);
+        }
+    };
+    ProjectInput.prototype.configure = function () {
+        this.element.addEventListener("submit", this.submitHandler);
+    };
     ProjectInput.prototype.attach = function () {
         this.hostElement.insertAdjacentElement("beforebegin", this.element);
     };
+    __decorate([
+        autoBind
+    ], ProjectInput.prototype, "submitHandler", null);
     return ProjectInput;
 }());
 var projectInput = new ProjectInput();
--- drag-and-drop/app.ts
+++ drag-and-drop/app.ts
@@ -1,3 +1,14 @@
+function autoBind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            return originalMethod.bind(this)
+        }
+    }
+    return adjustedDescriptor;
+}
+
 function $(selector: string, selectAll: boolean = false) {
     return selectAll ? document.querySelectorAll(selector) : document.querySelector(selector);
 }
@@ -6,15 +17,57 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = $("#project-input")! as HTMLTemplateElement;
-        this.hostElement = $("#id")! as HTMLDivElement;
+        this.hostElement = $("#app")! as HTMLDivElement;
         const importedNode = document.importNode(this.templateElement.content, true)
         this.element = importedNode.firstElementChild as HTMLFormElement;
-        console.log(this.element)
+        this.element.id = "user-input";
+
+        this.titleInputElement = this.element.querySelector("#title")! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector("#description")! as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector("#people")! as HTMLInputElement;
+
+        this.configure()
         this.attach()
 
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+
+        const enteredTitle = this.titleInputElement.value
+        const enteredDescription = this.descriptionInputElement.value
+        const enteredPeople = this.peopleInputElement.value
+
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || Number(enteredPeople) < 0) {
+            return alert("Please enter a valid input!")
+        }
+
+        return [enteredTitle, enteredDescription, Number(enteredPeople)]
+
+
+    }
+
+    @autoBind
+    private submitHandler(event: Event) {
+
+        event.preventDefault();
+        const userInput = this.gatherUserInput()
+        if (userInput) {
+
+            const [title, description, people] = userInput;
+            console.log(title, description, people)
+
+
+        }
+    }
+
+    private configure() {
+        this.element.addEventListener("submit", this.submitHandler)
     }
 
     private attach() {
